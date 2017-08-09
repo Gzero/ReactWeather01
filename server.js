3c7760c6d447695b1cfdc1a78632969670c9eafd
keeradit <keeradit@kitchenup.com>
2017-08-09 15:58:39 +0700

    Change var to const

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3009;
 
 app.use(function (req, res, next) {
     if(req.headers['x-forwarded-proto'] === 'http') {
