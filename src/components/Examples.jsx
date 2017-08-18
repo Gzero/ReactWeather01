aee94a8312ee97f45e930b082f9075bbd156b3ac
keeradit <keeradit@kitchenup.com>
2017-08-18 18:08:45 +0700

    Add foundation style to components

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
   return (
       <div>
-          <h3>Examples</h3>
-          <p>My message (example)</p>
-          <p>sub module</p>
+          <h3 className="text-center">Examples</h3>
+          <p>Here are a few example locations to try out</p>
+          <ol>
+              <li>
+                  <Link to="/?location=Philadephia">Philadephia, PA</Link>
+              </li>
+              <li>
+                  <Link to="/?location=Rio">Rio, Brazil</Link>
+              </li>
+          </ol>
       </div>
   )
 };
